54f7958dde93248ea6af435d6d8f49ad71a8d88c
Rajkamal <rajkamal.singh1011@gmail.com>
2022-03-10 01:54:44 +0530

    Created RepositoryDetailsActivity for the details of the commit and also added the search features.

--- app/src/main/java/com/hackthon/devfinder/Activities/UserDetails.java
+++ app/src/main/java/com/hackthon/devfinder/Activities/UserDetails.java
@@ -11,5 +11,11 @@ public class UserDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_details);
+
+
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/hackthon/devfinder/Models/GithubProfile.java
@@ -0,0 +1,78 @@
+package com.hackthon.devfinder.Models;
+
+public class GithubProfile {
+
+    private String name;
+    private String avatar;
+    private String starredUrl;
+    private String organisationUrl;
+    private String twitterName;
+    private String location;
+    private String bio;
+    private String htmlLink;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getStarredUrl() {
+        return starredUrl;
+    }
+
+    public void setStarredUrl(String starredUrl) {
+        this.starredUrl = starredUrl;
+    }
+
+    public String getOrganisationUrl() {
+        return organisationUrl;
+    }
+
+    public void setOrganisationUrl(String organisationUrl) {
+        this.organisationUrl = organisationUrl;
+    }
+
+    public String getTwitterName() {
+        return twitterName;
+    }
+
+    public void setTwitterName(String twitterName) {
+        this.twitterName = twitterName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getHtmlLink() {
+        return htmlLink;
+    }
+
+    public void setHtmlLink(String htmlLink) {
+        this.htmlLink = htmlLink;
+    }
+
+}
