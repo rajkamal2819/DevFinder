f9d03558f167f6a92032dfbd02e285614e3f0469
Rajkamal <rajkamal.singh1011@gmail.com>
2022-03-09 11:27:34 +0530

    Created The authentication section with the ui and signup and login section.

---
+++ app/src/main/java/com/hackthon/devfinder/Activities/Authentications.java
@@ -0,0 +1,45 @@
+package com.hackthon.devfinder.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
+import com.hackthon.devfinder.Adapters.FragmentAdapter;
+import com.hackthon.devfinder.MainActivity;
+import com.hackthon.devfinder.R;
+import com.hackthon.devfinder.databinding.ActivityAuthenticationsBinding;
+
+public class Authentications extends AppCompatActivity {
+
+    ViewPager viewPager;
+    TabLayout tabLayout;
+    ActivityAuthenticationsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityAuthenticationsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        viewPager = findViewById(R.id.viewpager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        viewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        binding.googleAuthButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Authentications.this, MainActivity.class));
+            }
+        });
+
+
+
+    }
+}
